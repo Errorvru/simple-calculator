f2574e29f5d33834ceba5c3a842c8d4098824b7a
Errorvru <vrushabhjain740@gmail.com>
2022-07-01 18:22:16 +0530

    Adv calculator

--- Main1.java
+++ Main1.java
@@ -7,18 +7,18 @@ class Main1 {
     Double number1, number2, result;
 
     // create an object of Scanner class
-  //  Scanner input = new Scanner(System.in);
+   Scanner input = new Scanner(System.in);
 
     // ask users to enter operator
- //   System.out.println("Choose an operator: +, -, *, /,^(power) or #(root)");
-    operator = 43;//input.next().charAt(0);
+  System.out.println("Choose an operator: +, -, *, /,^(power) or #(root)");
+    operator = input.next().charAt(0);
 
     // ask users to enter numbers
     System.out.println("Enter first number");
-    number1 = 25;//input.nextDouble();
+    number1 = input.nextDouble();
 
     System.out.println("Enter second number");
-    number2 = 10;//input.nextDouble();
+    number2 = input.nextDouble();
 
     switch (operator) {
 
